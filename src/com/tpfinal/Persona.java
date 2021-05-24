1239dd41e1401856c6f7a6568a8d43256446f576
Juan Ignacio Valiente <juanivaliente@outlook.com>
2021-05-24 11:30:42 -0300

    fix : Creacion de persona
    Se agrego excepciones y bucles al menú de creación de personas para evitar romper el programa.

--- src/com/tpfinal/Persona.java
+++ src/com/tpfinal/Persona.java
@@ -1,10 +1,9 @@
 package com.tpfinal;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Persona {
-
-    public static final char GENERO_POR_DEFECTO = 'H';
 
     private String nombre;
     private String apellido;
@@ -20,25 +19,58 @@ public class Persona {
     public void crearPersona(){
         Scanner scanner = new Scanner(System.in);
 
+        //Uso el replaceAll en caso de que el usuario ingrese numeros.
         System.out.println("Ingrese un nombre");
         this.nombre = scanner.next();
+        this.nombre = this.nombre.replaceAll("[^a-zA-Z]", "");
 
         System.out.println("Ingrese un apellido");
         this.apellido = scanner.next();
+        this.apellido = this.apellido.replaceAll("[^a-zA-Z]", "");
 
-        System.out.println("Ingrese su dni");
-        this.dni = scanner.nextInt();
+        while(this.dni == 0) {
+
+            //" limpio el buffer "
+            scanner.nextLine();
+
+            try {
+                System.out.println("Ingrese su dni");
+                this.dni = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Debe ingresar unicamente numeros");
+            }
+        }
 
         do{
             System.out.println("Ingrese su genero (H = Hombre, M = Mujer)");
             this.genero = scanner.next().charAt(0);
         }while (genero != 'H' && genero != 'M');
 
-        System.out.println("Ingrese su edad");
-        this.edad = scanner.nextInt();
+        while (this.edad == 0) {
 
-        System.out.println("Ingrese un celular");
-        this.celular = scanner.nextInt();
+            //" limpio el buffer "
+            scanner.nextLine();
+
+            try {
+                System.out.println("Ingrese su edad");
+                this.edad = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Debe ingresar unicamente numeros");
+            }
+        }
+
+        while (this.celular == 0) {
+
+            //" limpio el buffer "
+            scanner.nextLine();
+
+            try {
+                System.out.println("Ingrese un celular");
+                this.celular = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Debe ingresar unicamente numeros");
+            }
+        }
 
     }
 
